93b0040ca19b53cb99168dd110fbbf1b0246f9b2
strayforahona <98973803+strayforahona@users.noreply.github.com>
2022-02-05 18:58:56 +0530

    u

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -11,17 +11,19 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-gray-500 dark:text-gray-300">
         This is an open source word guessing game -{' '}
         <a
-          href="https://github.com/strayforahona/word-guessing-game/"
+          href="https://github.com/strayforahona/word-guessing-game"
           className="underline font-bold"
         >
-          check out the code here. 
-          For any queries, contact 
-          <a
+          check out the code here
+        </a>{' '}
+
+        For any queries, -{' '}
+        <j
           href="https://twitter.com/felixired"
           className="underline font-bold"
-        ></a>
+        >
           @felixired
-        </a>{' '}
+        </j>{' '}
       </p>
     </BaseModal>
   )
