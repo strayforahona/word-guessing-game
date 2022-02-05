2a290bca4e4ca8d9f55c2573072a2b6016f30688
strayforahona <98973803+strayforahona@users.noreply.github.com>
2022-02-05 14:44:53 +0530

    j

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const GAME_TITLE = 'STRAY KIDS WORDLE'
+export const GAME_TITLE = 'STRAy'
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
