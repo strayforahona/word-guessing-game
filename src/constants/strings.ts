afcf1247318c5da7046e2ef7801c6ef8235d5755
strayforahona <98973803+strayforahona@users.noreply.github.com>
2022-02-05 18:10:54 +0530

    s

---
+++ src/constants/strings.ts
@@ -0,0 +1,19 @@
+export const GAME_TITLE = 'STRAY KIDS WORDLE'
+
+export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const GAME_COPIED_MESSAGE = 'Result copied to clipboard. You can paste it anywhere now!'
+export const ABOUT_GAME_MESSAGE = 'About'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const WORD_NOT_FOUND_MESSAGE = 'Word not found (Sorry, we have made this on a limited bandwidth, so some legit words in the English language might not be recognised too!)'
+export const CORRECT_WORD_MESSAGE = (solution: string) =>
+  `The word for today was ${solution}`
+export const ENTER_TEXT = 'Enter'
+export const DELETE_TEXT = 'Delete'
+export const STATISTICS_TITLE = 'Statistics'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const NEW_WORD_TEXT = 'New word in'
+export const SHARE_TEXT = 'Share'
+export const TOTAL_TRIES_TEXT = 'Total tries'
+export const SUCCESS_RATE_TEXT = 'Success rate'
+export const CURRENT_STREAK_TEXT = 'Current streak'
+export const BEST_STREAK_TEXT = 'Best streak'
